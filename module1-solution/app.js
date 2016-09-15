9ea18558c33cc5308307415ce3b060051cb6ac7f
Oscar Moreno <ofmoreno06@gmail.com>
2016-09-15 00:28:01 -0500

    terminó items necesarios. hacer opcionales?

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,17 +4,34 @@
 // use strict for avoiding problems
 'use strict';
 
-// defining my application
+// defining my application - no dependencies
 angular.module('LunchCheck',[])
 .controller('LunchCheckController', LunchCheckController);
 
+// using the $inject property to protect services from minification
 LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope){
-  $scope.nombre = "oscar";
-  // $scope.lunchMenu = "";
-  // //$scope.check = function(){};
-  // $scope.message = "Hola";
 
+// making LunchCheckController function
+function LunchCheckController($scope){
+  $scope.placeholder1 = "list comma separated dishes you usually have for lunch";
+  $scope.menuList = "";
+  $scope.message = "";
+  $scope.note = "Empty items are not valid to count, i.e. (item1,,,item2) are just 2 items.";
+
+  $scope.check = function(listToCheck){
+    if (listToCheck != 0){
+      var items = chopString(listToCheck);
+      $scope.message = items <= 3 ? "Enjoy!" : "Too much!";
+    } else {
+        $scope.message = "Please enter data first";
+      }
+  };
+
+  var chopString = function(stringToChop){
+
+    var arrayOfStrings = stringToChop.split(/,\s*\w/g);
+    return arrayOfStrings.length;
+  };
 }
 
 })();
